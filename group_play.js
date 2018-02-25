50c4e75781fb5096bdea6bc2c870e0d745120b6b
Tavasya Agarwal <TavasyaAgarwal2020@u.northwestern.edu>
2018-02-24 21:28:44 -0600

    can't figure out modal

--- group_play.js
+++ group_play.js
@@ -2,3 +2,9 @@ function sendchat() {
 	document.getElementById("card-body").innerHTML = "Chris: " + document.getElementById("chat").value;
 	document.getElementById("chat").value = "";
 }
+
+function search() {
+	document.getElementById("secondlisting").innerHTML = document.getElementById("searchfield").value;
+	document.getElementById("searchfield").value = "";
+
+}
