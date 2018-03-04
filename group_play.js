92ccab50af98ba7cb62f40f851cb49c9e8a2ce3b
Tavasya Agarwal <TavasyaAgarwal2020@u.northwestern.edu>
2018-03-04 15:25:54 -0600

    group-play functions, and consistency

--- group_play.js
+++ group_play.js
@@ -1,5 +1,5 @@
 function sendchat() {
-	document.getElementById("card-body").innerHTML = "Chris: " + document.getElementById("chat").value;
+	document.getElementById("chat-1").innerHTML = "Chris: " + document.getElementById("chat").value;
 	document.getElementById("chat").value = "";
 }
 
@@ -7,4 +7,9 @@ function search() {
 	document.getElementById("secondlisting").innerHTML = document.getElementById("searchfield").value;
 	document.getElementById("searchfield").value = "";
 
+}
+
+function savegroup(){
+	document.getElementById("lastvisiblegroup").innerHTML = document.getElementById("name").value;
+	document.getElementById("name").value = "";
 }
